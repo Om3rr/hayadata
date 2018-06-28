64aee9fdf2c6fa976f72a542c80df36f5592b34e
Omer <Omertorrents@gmail.com>
2018-06-28 22:32:12 +0300

    asdfasdfasdf

--- dockerthings/app/algush.py
+++ dockerthings/app/algush.py
@@ -14,29 +14,31 @@ abstract_vectors = np.asarray(data['abstract vector'])
 title_vectors = np.asarray(data['title vector'])
 
 
-nb, d = abstract_vectors.shape                        
-nq = 10000                    
-index_abstract = faiss.IndexFlatL2(d)  
-index_abstract.add(abstract_vectors)     
+nb, d = abstract_vectors.shape
+nq = 10000
+index_abstract = faiss.IndexFlatL2(d)
+index_abstract.add(abstract_vectors)
 nb, d = title_vectors.shape
-index_titles = faiss.IndexFlatL2(d)  
-index_titles.add(title_vectors)    
+index_titles = faiss.IndexFlatL2(d)
+index_titles.add(title_vectors)
 
 
 def articles():
   samp = random.sample(range(0,len(data['abstract'])),30)
   return list(map(lambda x: {"title": data['title'][x], "abstract": data['abstract'][x], "index":x}, samp))
-  
-  
 
-def query(idx, by='title'):
+
+
+def query(idx, by='abstract'):
 	idx = int(idx)
 	to_query = index_titles if by == 'title' else index_abstract
-	dist, scores = index_titles.search(np.asarray([title_vectors[idx]]), 30)
+	dist, scores = index_abstract.search(np.asarray([abstract_vectors[idx]]), 30)
 	articles = []
 	scores = scores[0]
 	dist = dist[0]
 	for d,s in zip(dist, scores):
+		title = data['title'][s]
+		abstract = data['abstract'][s]
+		idx = int(s)
 		articles.append({"title": data['title'][s], "abstract": data['abstract'][s], "index":int(s)})
 	return articles
-	
--- dockerthings/app/app.py
+++ dockerthings/app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from algush import articles, query
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='dist',)
 
 @app.route('/api/articles')
 def get_atricles():
@@ -10,7 +10,11 @@ def get_atricles():
 def query_article():
 	article_idx = request.args.get('article')
 	return jsonify(query(article_idx))
-	
-	
+
+@app.route('/<path:path>')
+def static_file(path):
+    print(path)
+    return app.send_static_file(path)
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
